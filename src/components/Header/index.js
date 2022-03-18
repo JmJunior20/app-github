42eef2c9e6d8dac73f962acc0ddd2a1f27692859
Jose Milton Junior <jmjuniormonteiro@gmail.com>
2022-03-18 15:54:44 -0300

    Adicionando a logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+
+import "./style.css";
+import Logo from "../imagens/logo-jmdeveloper1.png";
 
 const Header = (props) => {
-    return (
-        <h1>{props.children}</h1>
-    )
-}
+  return (
+    <div className="header">
+      <img src={Logo} alt="Logo JmJuniorDeveloper" />
+      <h1>{props.children}</h1>
+    </div>
+  );
+};
 
 export default Header;
